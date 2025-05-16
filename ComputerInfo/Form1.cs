3d7ccd294c37b547de34b8000680f922c48f3b32
raduoosiesebre <rivanov@iesebre.com>
2025-05-16 13:47:00 +0200

    Agregar archivos de proyecto.

---
+++ ComputerInfo/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ComputerInfo
+{
+    public partial class Form1 : Form
+    {
+        private NotifyIcon trayIcon;
+        public Form1()
+        {
+            InitializeComponent();
+
+            trayIcon = new NotifyIcon();
+            trayIcon.Icon = SystemIcons.Information;
+            trayIcon.Visible = true;
+
+            string computerName = Environment.MachineName;
+            string osInfo = $"{Environment.OSVersion.VersionString} {(Environment.Is64BitOperatingSystem ? "x64" : "x86")}";
+            string trayText = $"PC: {computerName} | OS Info: {osInfo}";
+
+            // Limita a 63 caràcters
+            if (trayText.Length > 63)
+                trayText = trayText.Substring(0, 63);
+
+            trayIcon.Text = trayText;
+
+            trayIcon.BalloonTipTitle = "Dades de l'ordinador";
+            trayIcon.BalloonTipText = $"Nom: {computerName}\nSO: {osInfo}";
+            trayIcon.ShowBalloonTip(3000);
+
+            var contextMenu = new ContextMenu();
+            contextMenu.MenuItems.Add("Exit", (s, e) => Application.Exit());
+            trayIcon.ContextMenu = contextMenu;
+
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            trayIcon.Visible = false; // Hide tray icon when closing the form
+            base.OnFormClosing(e);
+        }
+    }
+}
---
+++ ComputerInfo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ComputerInfo
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new TrayAppContext());
+        }
+    }
+
+    public class TrayAppContext : ApplicationContext
+    {
+        private NotifyIcon trayIcon;
+
+        public TrayAppContext()
+        {
+            string computerName = Environment.MachineName;
+            string osInfo = $"{Environment.OSVersion} (64-bit: {Environment.Is64BitOperatingSystem})";
+            string trayText = $"PC: {computerName} | OS Info: {osInfo}";
+
+            trayText = trayText.Replace("\n", " ").Replace("\r", "");
+            if (trayText.Length > 63)
+                trayText = trayText.Substring(0, 63);
+
+            trayIcon = new NotifyIcon()
+            {
+                Icon = SystemIcons.Information,
+                Visible = true,
+                Text = trayText,
+                ContextMenu = new ContextMenu(new MenuItem[]
+                {
+                    new MenuItem("Exit", (s, e) => ExitThread())
+                })
+            };
+        }
+
+        protected override void ExitThreadCore()
+        {
+            trayIcon.Visible = false;
+            trayIcon.Dispose();
+            base.ExitThreadCore();
+        }
+    }
+}
